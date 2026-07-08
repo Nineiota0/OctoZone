133657cbbe684f4c42317ba0f92bcb0b48f3c3d2
Nineiota0 <89941342+Nineiota0@users.noreply.github.com>
2026-07-08 04:15:32 -0700

    feat: add pathfinder class algorithm

---
+++ include/octozone/Pathfinder.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "octozone/Grid.hpp"
+#include "octozone/Path.hpp"
+#include "octozone/Position.hpp"
+
+namespace octozone 
+{
+
+    class Pathfinder 
+    {
+        public:
+            static Path findPath(const Grid& grid, Position start, Position goal);
+    };
+
+}
