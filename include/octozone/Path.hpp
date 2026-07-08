05320247f14344cd8fced489b7d5eff69eaa9243
Nineiota0 <89941342+Nineiota0@users.noreply.github.com>
2026-07-08 04:00:37 -0700

    feat: add path type

---
+++ include/octozone/Path.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "octozone/Position.hpp"
+#include <vector>
+
+namespace octozone
+{
+    using Path = std::vector<Position>;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include "octozone/Game.hpp"
+#include "octozone/Path.hpp"
+
+#include <iostream>
 
 int main()
 {
-    octozone::Game game;
-    game.run();
+    octozone::Path path;
+
+    path.push_back({9, 0});
+    path.push_back({8, 0});
+    path.push_back({7, 0});
+
+    for (const auto& position : path)
+    {
+        std::cout << "("
+                  << position.row
+                  << ", "
+                  << position.col
+                  << ")\n";
+    }
 
     return 0;
 }
